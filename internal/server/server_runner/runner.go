f5beee3018a47363057e526af0711b26b62dc798
Eduardo Faria <115400010+2101dudu@users.noreply.github.com>
2024-11-01 17:04:22 +0000

    fix: updated runner.go

--- internal/server/server_runner/runner.go
+++ internal/server/server_runner/runner.go
@@ -4,8 +4,7 @@ import sc "nms/internal/server/server_config"
 
 func main() {
 	go sc.StartTCPServer("8080")
-	go sc.StartUDPServer("8081")
+    sc.StartUDPServer("8081")
 
-	// O QUE É ISTO????
 	select {} // Bloqueio para manter o servidor rodando
 }
