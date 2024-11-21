73cb788876df6ad8fd0a823bfd86b3b74c18adad
Eduardo Faria <eduardo21faria@gmail.com>
2024-11-21 21:43:08 +0000

    feat: added aux function and modularized code

---
+++ internal/agent/udp/handlePacket.go
@@ -0,0 +1,23 @@
+package udp
+
+import (
+	"fmt"
+	"net"
+	packet "nms/pkg/packet"
+	utils "nms/pkg/utils"
+)
+
+func handlePacket(packetType utils.MessageType, packetPayload []byte, conn *net.UDPConn) {
+	switch packetType {
+	case utils.ACK:
+		packet.HandleAck(packetPayload, packetsWaitingAck, &pMutex, agentID, conn)
+		return
+	case utils.TASK:
+		fmt.Println("[AGENT] Metrics received from server")
+		// HandleTask method - TO DO
+		return
+	default:
+		fmt.Println("[AGENT] [ERROR 7] Unknown message type received from server")
+		return
+	}
+}
