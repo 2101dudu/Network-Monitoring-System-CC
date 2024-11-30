eb15e61a9738944a661bfff1dd45271689984966
Eduardo Faria <115400010+2101dudu@users.noreply.github.com>
2024-11-30 18:03:53 +0000

    fix: findInLines function (#42)

--- internal/agent/udp/findInLines.go
+++ internal/agent/udp/findInLines.go
@@ -2,18 +2,14 @@ package udp
 
 import "strings"
 
-func findInLines(containsStrings []string, output string) string {
+func findInLines(containsString string, output string) string {
 	lines := strings.Split(output, "\n")
 
-	final := ""
-
 	for _, line := range lines {
-		for _, containsString := range containsStrings {
-			if strings.Contains(line, containsString) {
-				final += line + "\n"
-			}
+		if strings.Contains(line, containsString) {
+			return line
 		}
 	}
 
-	return final
+	return ""
 }
