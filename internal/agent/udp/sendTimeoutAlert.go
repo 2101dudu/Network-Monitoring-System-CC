66e5a0b65ec91ea184e5948247810ccf75d1637e
Eduardo Faria <eduardo21faria@gmail.com>
2024-12-04 15:16:53 +0000

    fix: code redability

--- internal/agent/udp/sendTimeoutAlert.go
+++ internal/agent/udp/sendTimeoutAlert.go
@@ -12,12 +12,7 @@ import (
 func sendTimeoutAlertAndEmptyMetrics(command string, taskID uint16, agentID byte, startTime time.Time) {
 	// Send alert for timeout
 	newPacketID := utils.ReadAndIncrementPacketID(&packetID, &packetMutex, true)
-	buildAlert := alert.NewAlertBuilder().
-		SetPacketID(newPacketID).
-		SetSenderID(agentID).
-		SetTaskID(taskID).
-		SetAlertType(alert.TIMEOUT).
-		SetTime(startTime.Format("15:04:05.000000000"))
+	buildAlert := alert.NewAlertBuilder().SetPacketID(newPacketID).SetSenderID(agentID).SetTaskID(taskID).SetAlertType(alert.TIMEOUT).SetTime(startTime.Format("15:04:05.000000000"))
 
 	newAlert := buildAlert.Build()
 	tcp.ConnectTCPAndSendAlert(utils.SERVERTCP, newAlert)
@@ -25,14 +20,7 @@ func sendTimeoutAlertAndEmptyMetrics(command string, taskID uint16, agentID byte
 	// Send empty metrics packet
 	serverConn := utils.ResolveUDPAddrAndDial("localhost", "8081")
 	metricsID := utils.ReadAndIncrementPacketID(&packetID, &packetMutex, true)
-	newMetrics := metrics.NewMetricsBuilder().
-		SetPacketID(metricsID).
-		SetAgentID(agentID).
-		SetTaskID(taskID).
-		SetTime(startTime.Format("15:04:05.000000000")).
-		SetCommand(command).
-		SetMetrics("").
-		Build()
+	newMetrics := metrics.NewMetricsBuilder().SetPacketID(metricsID).SetAgentID(agentID).SetTaskID(taskID).SetTime(startTime.Format("15:04:05.000000000")).SetCommand(command).SetMetrics("").Build()
 
 	hash := metrics.CreateHashMetricsPacket(newMetrics)
 	newMetrics.Hash = (string(hash))
